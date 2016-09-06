33ec3c2b30a08b60316ff1f6708f30b052dbf7e4
Mario Alberto Barragán Espinosa <albeto8@gmail.com>
2016-09-05 21:26:33 -0500

    Sign in custom classes complete

---
+++ Social Media/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social Media
+//
+//  Created by Mario Alberto Barragán Espinosa on 05/09/16.
+//  Copyright © 2016 mario. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
