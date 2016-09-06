c85d625986cb61be65a0afcd1b803ce987f9ba1e
Mario Alberto Barragán Espinosa <albeto8@gmail.com>
2016-09-05 19:07:24 -0500

    Completed setting up Firebase

---
+++ Social Media/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Media
+//
+//  Created by Mario Alberto Barragán Espinosa on 05/09/16.
+//  Copyright © 2016 mario. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
