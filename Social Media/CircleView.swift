49ffd767302f7498b0bf63350431913482598ca3
Mario Alberto Barragán Espinosa <albeto8@gmail.com>
2016-09-06 14:29:09 -0500

    style. News feed UI

--- Social Media/CircleView.swift
+++ Social Media/CircleView.swift
@@ -1,24 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  Social Media
 //
-//  Created by Mario Alberto Barragán Espinosa on 05/09/16.
+//  Created by Mario Alberto Barragán Espinosa on 06/09/16.
 //  Copyright © 2016 mario. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
-    override func awakeFromNib() {
+    override func awakeFromNib(){
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-        
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
